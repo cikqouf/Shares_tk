13f6adacadf43a04652901cddeda28131c8e5670
koiyui <3831269l@gmail.com>
2023-01-20 23:06:07 +0800

    __init__修改

--- Shares_tk/__init__.py
+++ Shares_tk/__init__.py
@@ -9,5 +9,3 @@ usage::
 
 """
 from Shares_tk.shares_tk import Stock_tk
-from Shares_tk.chart_matplotlib_ver import technic_chart_matplotlib
-from Shares_tk.chart_plotly_ver import technic_chart_plotly
